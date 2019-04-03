86ea7a42aa9f46341a4ad6329a53789053c6b2d0
David Collom <david@collom.co.uk>
2019-04-03 22:23:17 +0100

    Improve build script to be better read output

--- build.sh
+++ build.sh
@@ -12,30 +12,27 @@ then
     exit 1
 fi
 
-IMAGES=$(find * -maxdepth 1 -type d)
-# IMAGES=(unifi-exporter)
-# IMAGES=(bind9-exporter)
+# IMAGES=$(find * -maxdepth 1 -type d)
+IMAGES=${@:2}
 
 # Loop through all the directories
 for img in $IMAGES; do
-    pushd $img
-    if [ -f .skip ]; then
+    echo "Building ${img}..."
+    if [ -f ${img}/.skip ]; then
         echo "Skipping building of ${img} due to .skip file present"
-        popd
         continue
     fi
-    version=$(cat VERSION 2>/dev/null)
+    version=$(cat ${img}/VERSION 2>/dev/null)
     imgname="${REPO}/${img}:${version:-latest}"
     all_imgs=""
-    dckr_images=$(find . -iname 'Dockerfile*')
-    add_images=$(find . -iname 'image*')
-    multi_arch=$(find . -iname 'Dockerfile*' -or -iname 'image*' | wc -l | sed -e 's/ //g')
+    dckr_images=$(find $img -iname 'Dockerfile*')
+    add_images=$(find $img -iname 'image*')
+    multi_arch=$(find $img -iname 'Dockerfile*' -or -iname 'image*' | wc -l | sed -e 's/ //g')
 
     if [[ $multi_arch == '1' ]]; then
         echo "Not a Multi-Arch directory - standard build/push"
-        docker build --build-arg VERSION=${version:-latest} -t $imgname -f ${dckr_images[0]} .
-        docker push $imgname
-        popd
+        docker build --build-arg VERSION=${version:-latest} -t $imgname -f ${dckr_images[0]} $img/ | sed "s/^/\[$img\] /"
+        docker push $imgname  | sed "s/^/\[$img\] /"
         continue
     fi
 
@@ -44,9 +41,9 @@ for img in $IMAGES; do
     for dckr_file in $dckr_images; do
         arch="${dckr_file##*.}"
         # Build Image
-        docker build --pull --build-arg VERSION=${version:-latest} -t "${imgname}-${arch}" -f $dckr_file .
+        docker build --pull --build-arg VERSION=${version:-latest} -t "${imgname}-${arch}" -f $dckr_file $img/ | sed "s/^/\[$img\] /"
         # Push Image (Needs to exist for creating manifests)
-        docker push "${imgname}-${arch}"
+        docker push "${imgname}-${arch}" | sed "s/^/\[$img\] /"
         all_imgs+="${imgname}-${arch} "
     done
     for addimg in $add_images; do
@@ -55,21 +52,20 @@ for img in $IMAGES; do
     done
 
     # Create the Manifest 
-    docker manifest create --amend ${imgname} ${all_imgs}
+    docker manifest create --amend ${imgname} ${all_imgs} | sed "s/^/\[$img\] /"
 
     # We need to loop back again
     # So that we can annotate the images with os/arch
     for dckr_file in $dckr_images; do
         arch="${dckr_file##*.}"
-        docker manifest annotate ${imgname} "${imgname}-${arch}" --os linux --arch=${arch}
+        docker manifest annotate ${imgname} "${imgname}-${arch}" --os linux --arch=${arch} | sed "s/^/\[$img\] /"
     done
 
     for add_img in $add_images; do
         arch="${add_img##*.}"
         addimgname=$(cat ${add_img})
-        docker manifest annotate ${imgname} "${addimgname}" --os linux --arch=${arch}
+        docker manifest annotate ${imgname} "${addimgname}" --os linux --arch=${arch} | sed "s/^/\[$img\] /"
     done
     # Finally push the manifest to the hub
-    docker manifest push -p ${imgname}
-    popd
+    docker manifest push -p ${imgname} | sed "s/^/\[$img\] /"
 done

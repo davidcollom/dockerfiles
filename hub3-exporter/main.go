a877e3c9f9cc8cd4c301fa229e8778d40a91dc9b
David Collom <david@collom.co.uk>
2020-02-28 21:50:37 +0000

    Adding in vmhub3 exporter for prometheus

---
+++ hub3-exporter/main.go
@@ -0,0 +1,11 @@
+--2019-11-24 12:55:23--  https://raw.githubusercontent.com/welbymcroberts/hub3_exporter/master/hub3_exporter.go
+Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 151.101.0.133, 151.101.192.133, 151.101.64.133, ...
+Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|151.101.0.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 9450 (9.2K) [text/plain]
+Saving to: ‘hub3_exporter.go’
+
+     0K .........                                             100% 10.8K=0.9s
+
+2019-11-24 12:55:28 (10.8 KB/s) - ‘hub3_exporter.go’ saved [9450/9450]
+

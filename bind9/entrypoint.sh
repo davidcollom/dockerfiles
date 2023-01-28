e640644fb9d46266554ea4da7d749414e065f896
David Collom <david@collom.co.uk>
2023-01-28 19:54:52 +0000

    Adding some logging

--- bind9/entrypoint.sh
+++ bind9/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+echo "Setting Permissions..."
 chown -R named:named /var/cache/bind
+
+echo "Starting Named..."
 # Run in foreground and log to STDERR (console):
 exec /usr/sbin/named -c /etc/bind/named.conf -f -u named

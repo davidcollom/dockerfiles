eb416daece9094de9f2b6fe6d66daaa22b18dc85
David Collom <david@collom.co.uk>
2023-01-28 19:51:56 +0000

    Ensure Exec for signals

--- bind9/entrypoint.sh
+++ bind9/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 chown -R named:named /var/cache/bind
 # Run in foreground and log to STDERR (console):
-/usr/sbin/named -c /etc/bind/named.conf -f -u named
+exec /usr/sbin/named -c /etc/bind/named.conf -f -u named
